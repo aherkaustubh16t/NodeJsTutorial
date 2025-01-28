a8461673b29263e085e9c90e964e81920acc2d0d
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-01-29 01:35:40 +0530

    First server

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  console.log(req);
+  res.write("<h1>Kaustubh</h1>")
 }).listen(3000, () => {
   console.log("Server Started");
   console.log("Server running on http://localhost:3000");
