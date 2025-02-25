35d889640230d8c78c47988b59a77abc86bca69a
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-02-25 13:45:51 +0530

    url parsing

--- Routing/index.js
+++ Routing/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
-
+const fs = require('fs');
+const url = require('url');
 // function dataControl(req, res) {
 //   res.write("Hieee");
 //   res.end();
@@ -7,14 +8,25 @@ const http = require('http');
 
 // http.createServer(dataControl).listen(4500);
 
+let products = JSON.parse(fs.readFileSync('../Data/product.json', 'utf-8'))
+let productHtml = fs.readFileSync('../Data/index.html', 'utf-8');
+
+let producthtmlArray = products.map((product) => {
+  let output = productHtml.replaceAll('{{%name%}}', product.name);
+  output = output.replace('{{%id%}}', product.id);
+
+  return output;
+})
 http.createServer((req, res) => {
 
-  const path = req.url;
+  let { query, pathname: path } = url.parse(req.url, true);
+  // const path = req.url;
+  // console.log(x);
   console.log("New Request Received");
 
   if (path === "/") {
     res.writeHead(200);
-    res.end("<h1>You are in Home Page</h1>");
+    res.end("Home Page");
   }
   else if (path === "/about") {
     res.writeHead(200);
@@ -23,6 +35,22 @@ http.createServer((req, res) => {
   else if (path === "/contact") {
     res.writeHead(200);
     res.end("<h1>You are in Contact Page</h1>");
+  }
+  else if (path === "/product") {
+
+    if (query.id) {
+      res.end("Product with id = " + query.id)
+    }
+    else {
+
+      res.writeHead(200, {
+        'content-type': 'text/html'
+      });
+
+      res.write('Products List')
+      res.end(producthtmlArray.join(','));
+      console.log();
+    }
   }
   else {
     res.writeHead(404);
